ecfd279929c2c5c56c886777b66e428c64649b06
Rodrigo Jose Sarmento Oliveira <rodrigojsoliveira@gmail.com>
2019-11-18 22:33:38 -0300

    Criação da classe Produto e relacionamento muitos para muitos.

--- src/main/java/com/rjso/cursomc/CursomcApplication.java
+++ src/main/java/com/rjso/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rjso.cursomc.domain.Categoria;
+import com.rjso.cursomc.domain.Produto;
 import com.rjso.cursomc.repositories.CategoriaRepository;
+import com.rjso.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,7 +31,19 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00d);
+		Produto p2 = new Produto(null, "Impressora", 800.00d);
+		Produto p3 = new Produto(null, "Mouse", 80.00d);
+		
+		cat1.setProdutos(Arrays.asList(p1, p2, p3));
+		cat2.setProdutos(Arrays.asList(p2));
+		
+		p1.setCategorias(Arrays.asList(cat1));
+		p2.setCategorias(Arrays.asList(cat1, cat2));
+		p3.setCategorias(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
--- src/main/java/com/rjso/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/rjso/cursomc/repositories/ProdutoRepository.java
@@ -2,9 +2,9 @@ package com.rjso.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.rjso.cursomc.domain.Categoria;
+import com.rjso.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
 }
