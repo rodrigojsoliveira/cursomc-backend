74fb084c2324c5fe27a0fb8813a37fb53966917e
Rodrigo Jose Sarmento Oliveira <rodrigojsoliveira@gmail.com>
2019-12-07 23:13:08 -0300

    Criação de um retorno de validação personalizado.

--- src/main/java/com/rjso/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/rjso/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package com.rjso.cursomc.resources.exceptions;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.rjso.cursomc.services.exceptions.CategoriaNaoEncontradaException;
@@ -30,6 +32,22 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validacaoDTO(MethodArgumentNotValidException e,
+			HttpServletRequest request) {
+
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de Validação", System.currentTimeMillis());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+
+	}
+	
+	
 
 	@ExceptionHandler(PedidoNaoEncontradoException.class)
 	public ResponseEntity<StandardError> pedidoNaoEncontrado(PedidoNaoEncontradoException e,
