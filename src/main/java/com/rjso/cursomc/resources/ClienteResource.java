28881bb3b9cf166c525886f8ad08211f13170887
Rodrigo Jose Sarmento Oliveira <rodrigojsoliveira@gmail.com>
2019-12-11 23:40:01 -0300

    Criação do método POST para inserção de clientes.

--- src/main/java/com/rjso/cursomc/resources/ClienteResource.java
+++ src/main/java/com/rjso/cursomc/resources/ClienteResource.java
@@ -1,8 +1,11 @@
 package com.rjso.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.rjso.cursomc.domain.Cliente;
 import com.rjso.cursomc.dto.ClienteDTO;
+import com.rjso.cursomc.dto.ClienteNewDTO;
 import com.rjso.cursomc.services.ClienteService;
 
 @RestController
@@ -64,6 +70,18 @@ public class ClienteResource {
 		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
 
 		return ResponseEntity.ok(listDTO);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		// Pegar o URI do novo recurso que foi inserido no banco de dados.
+		// Este URI ficará no padrão /categorias/{id do novo recurso criado}.
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		// Retorna o response com o URI do novo recurso e o código http 201 indicando
+		// que o recurso foi gerado com sucesso.
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/rjso/cursomc/services/ClienteService.java
+++ src/main/java/com/rjso/cursomc/services/ClienteService.java
@@ -2,34 +2,45 @@ package com.rjso.cursomc.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rjso.cursomc.domain.Cidade;
 import com.rjso.cursomc.domain.Cliente;
+import com.rjso.cursomc.domain.Endereco;
+import com.rjso.cursomc.domain.enums.TipoCliente;
 import com.rjso.cursomc.dto.ClienteDTO;
+import com.rjso.cursomc.dto.ClienteNewDTO;
 import com.rjso.cursomc.repositories.ClienteRepository;
+import com.rjso.cursomc.repositories.EnderecoRepository;
 import com.rjso.cursomc.services.exceptions.ClienteNaoEncontradoException;
 import com.rjso.cursomc.services.exceptions.IntegridadeDeDadosException;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
-	
+
+	@Autowired
+	private EnderecoRepository enderecoRepo;
+
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public Cliente find(Integer id) {
-		Optional<Cliente> obj =  repo.findById(id);
-		return obj.orElseThrow(() -> new ClienteNaoEncontradoException("Cliente não encontrado. Id: " + id +
-				" Tipo: " + Cliente.class.getName()));
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ClienteNaoEncontradoException(
+				"Cliente não encontrado. Id: " + id + " Tipo: " + Cliente.class.getName()));
 	}
-	
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
@@ -49,15 +60,48 @@ public class ClienteService {
 		PageRequest pageRequest = PageRequest.of(pagina, linhasPorPagina, Direction.valueOf(direcao), ordenaPor);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
-	
+
+	public Cliente fromDTO(ClienteNewDTO objNewDTO) {
+		Cliente cliente = new Cliente(null, objNewDTO.getNome(), objNewDTO.getEmail(), objNewDTO.getCpfOuCnpj(),
+				TipoCliente.toEnum(objNewDTO.getTipoCliente()));
+
+		Cidade cidade = new Cidade(objNewDTO.getCidadeId(), null, null);
+
+		Endereco endereco = new Endereco(null, objNewDTO.getLogradouro(), objNewDTO.getNumero(),
+				objNewDTO.getComplemento(), objNewDTO.getBairro(), objNewDTO.getCep(), cidade, cliente);
+
+		cliente.getTelefones().add(objNewDTO.getTelefone1());
+
+		if (objNewDTO.getTelefone2() != null) {
+			cliente.getTelefones().add(objNewDTO.getTelefone2());
+		}
+
+		if (objNewDTO.getTelefone3() != null) {
+			cliente.getTelefones().add(objNewDTO.getTelefone3());
+		}
+
+		cliente.getEnderecos().add(endereco);
+
+		return cliente;
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
 
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		// Se o id do objeto for diferente de null, o repositório irá atualizar os
+		// dados, e não criar uma nova categoria.
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepo.saveAll(obj.getEnderecos());
+		return obj;
+	}
 
 }
